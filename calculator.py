c51fca01d352daef0ffc103d997346c9a2f6e622
remigiusM <garethremigius@gmail.com>
2024-09-07 23:59:22 +0100

    assignment

---
+++ calculator.py
@@ -0,0 +1,20 @@
+a = int(input("Enter a number:"))
+b = int(input("Enter another number: "))
+
+
+def add():
+    return a + b
+
+def subtract():
+    return a - b
+
+def divide():
+    return a / b
+
+def multiply():
+    return a * b
+
+print(add())
+print(subtract())
+print(divide())
+print(multiply())
